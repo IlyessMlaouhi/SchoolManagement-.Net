a7868a22ee82dc9e9198dd61f3a989467c5c0aab
Amen-Haj-Brahim <amen.brahim@pristini-international.tn>
2026-07-27 13:41:39 +0100

    first commit

---
+++ SchoolManagement/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Models;
+namespace SchoolManagement.Data;
+
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Teacher> Teachers => Set<Teacher>();
+}
---
+++ SchoolManagement/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolManagement.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
